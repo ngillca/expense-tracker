77982c8631cb1c1bc01ace2a540dfae588c2b2e8
ngillca <sngill.ca@gmail.com>
2021-12-04 18:37:07 -0800

    Testing color theme in UI

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,38 +1,39 @@
-import React, { useState } from 'react'
-import './ExpenseItem.css'
-import ExpenseDate from './ExpenseDate'
-import Card from '../UI/Card'
-import Dashboard from '../UI/Dashboard'
+import React, { useState } from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import Dashboard from "../UI/Dashboard";
 
 export default function ExpenseItem(props) {
-    
-    
-    
-    
-    // array destructuring [0] current title [1] function name to set new title
-    const [title, setTitle] = useState(props.title)
-    // temporarily changes value until next reload
-    // but when state changes React will reload the current component
-    const clickHandler = () => {
-        setTitle('Paid')
-    }
+  // array destructuring [0] current title [1] function name to set new title
+  const [title, setTitle] = useState(props.title);
+  // temporarily changes value until next reload
+  // but when state changes React will reload the current component
+  const clickHandler = () => {
+    setTitle("Paid");
+  };
 
-
-    return (
-        <>
-        
-        <Card  className="expense-item">
-        
-            <ExpenseDate date={props.date} />
+  return (
+    <>
+      <Card className="expense-item">
+        <ExpenseDate id="oop" date={props.date} />
         <div className="expense-item__description ">
-            <h2>{ title }</h2>
-            <div className="expense-item__price">${ props.amount }</div>
+          <h2>{title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button style={{'backgroundColor': 'black', color: 'white', border: '1px solid black', 'borderRadius': '6px', padding: '6px'}} onClick={ clickHandler }>Pay</button>
-        <div style={{padding: '6px', color: 'coral', border: '1px solid black', 'borderRadius': '6px', marginLeft: '20px'}}>
-        <Dashboard />
-        </div>
-        </Card>
-        </>
-    )
+        <button
+          style={{
+            backgroundColor: "black",
+            color: "white",
+            border: "1px solid black",
+            borderRadius: "6px",
+            padding: "6px",
+          }}
+          onClick={clickHandler}
+        >
+          Pay
+        </button>
+      </Card>
+    </>
+  );
 }
