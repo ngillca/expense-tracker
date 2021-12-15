da932d04068f74e5bed63d11cd43743241a30796
ngillca <sngill.ca@gmail.com>
2021-12-15 12:40:37 -0800

    Metrics adjustment in chart component

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -5,20 +5,18 @@ export default function ChartBar(props) {
 
     let barFillHeight = '0%';
 
-    if (props.max > 0) {
+    if (props.maxValue > 0) {
         barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
     }
 
     return (
         <div className="chart-bar">
             <div className="chart-bar__inner">
-                <div className="char-bar__fill" style={{ height: barFillHeight }}>
+                <div className="char-bar__fill" style={{ height: '10rem' }}>
 
                 </div>
             </div>
-            <div className="chart-bar__label">
-                    
-            </div>
+            <div className="chart-bar__label">{ props.label }</div>
         </div>
     )
 }
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -25,7 +25,7 @@ export default function ExpenseForm(props) {
 
     const data = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate)
     };
 
