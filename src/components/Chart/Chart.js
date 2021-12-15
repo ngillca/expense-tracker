7d0b379753da9e4fd55d8448fb6740016c5bbf65
ngillca <sngill.ca@gmail.com>
2021-12-15 12:10:05 -0800

    Adding chart component in UI

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ChartBar from './ChartBar'
+import './Chart.css'
+
+export default function Chart() {
+    return (
+        <div className="chart">
+            {props.dataPoints.map(dataPoint => 
+                <ChartBar 
+                  key={dataPoint.label} 
+                  value={dataPoint.value} 
+                  maxValue={null} 
+                  label={dataPoint.label}
+                />)} 
+        </div>
+    )
+}
